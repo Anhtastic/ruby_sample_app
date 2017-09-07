9b955439bfb1fa55a8a36d7bd75db1424eeb4925
Anh Doan <anhdastic@gmail.com>
2017-09-07 16:45:31 -0700

    Finish user signup, one test fail, no idea why something around render_to, Chapter 7 completed

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ RubySampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # speed up tests by lowering bycrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true 
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name		"Anh Doan"
+		email		"anhdastic@gmail.com"
+		password	"foobar"
+		password_confirmation	"foobar"
+	end
+end
